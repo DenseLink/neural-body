173768ce42063a281333f403f718fe9b89cf238e
Craig Boger <46507697+craigboger@users.noreply.github.com>
2020-06-24 14:46:35 -0400

    Added Cory's fix for the trails and updated code formatting a bit.  Still needs comments and other formatting changes.  TODOs added for some questions throughout grav2.py.

--- 0_demo-sim/grav2.py
+++ 0_demo-sim/grav2.py
@@ -29,19 +29,20 @@ pygame.init()
 
 
 def main():
-    # setting screen size
+    # Set screen size and launch window
     scr_width = 1000
     scr_height = 700
     screen = pygame.display.set_mode((scr_width, scr_height))
 
-    # setting caption
-    pygame.display.set_caption("Elliptical orbit")
+    # Setting caption at top of window
+    pygame.display.set_caption("AstroGators - Neural Body Simulator")
 
-    # creating clock variable
+    # Creating clock variable.
+    # Helps with tracking time and managing framerate.
     clock = pygame.time.Clock()
 
     # trackers for the planet trails
-    num_planets = 9
+    num_planets = 13  #TODO: Initialize the number of bodies from the config file.  Are we just adding inner view as other planets?
     tail_length = 150
     orbits(screen, num_planets, tail_length, clock, scr_width, scr_height)
 
@@ -49,6 +50,19 @@ def main():
 # simple function for putting the sun on the screen at a given coordinate. In the future, this style will be used for
 # the planets and the png that keeps their dark side shadowed
 def sun(screen, x, y):
+    """
+    A simple function for putting the sun on the screen at a given coordinate.
+
+    TODO: In the future, this style will be used for the planets and the png that keeps their dark side shadowed.
+
+    Args:
+        screen: Output display.
+        x:
+        y:
+
+    Returns:
+
+    """
     sun_img = pygame.image.load('sun.png')
     sun_img = pygame.transform.scale(sun_img, (10, 10))
     screen.blit(sun_img, (x, y))
@@ -57,91 +71,102 @@ def sun(screen, x, y):
 def orbits(screen, num_planets, tail_length, clock, scr_width, scr_height):
     x_track = [[0] * tail_length for i in range(num_planets)]
     y_track = [[0] * tail_length for i in range(num_planets)]
+
     pause = 0
     view = 0
-    speed = 1
     click_now = 0
     input_active = 0
     textbox_active = 0
     input_text = ""
-    # speed = 10  # 1 is about a 35 second year (earth)
 
-    zoomFactor = 15  # 15 is the max to show all planets... 1 is the min to show only through mars
-    zoom = 1000000000 * zoomFactor
-    time_step = 750000 * speed
+    # Factor to setup view magnification for all planets.
+    # Ranges from 15 to show all planets down to 1 to show only through Mars.
+    # Zooms are later used for scaling views from simulation space to the view
+    # of the inner planets and view of the outer planet.
+    zoom_factor = 15
+    zoom = 1000000000 * zoom_factor
+    # Set inner planet view zoom
+    zoom_i = 15000000000
+    # Set time step and speed factor.
+    # 1 is about a 35 second year (earth)
+    speed = 1
+    # TODO: Figure out time step
+    # time_step = 750000 * speed
+    time_step = 118800 * speed
+    # Keep track of how many simulation time steps have passed.
     curr_time_step = 0
 
-    simulation = BenrulesRealTimeSim(time_step=time_step, in_config_df = pd.read_csv("mars_sim_config.csv"))
-                                     #nn_path="Predict-NN-Deploy-V1.02-LargeDataset_2-layer_selu_lecun-normal_mae_Adam_lr-1e-6_bs-128_epoch-250.h5")
-
+    # Create simulation object that keeps track of simultion state and predicts
+    # position of satellite specified in config file.
+    # TODO: Get CSV location from prompt and load CSV file to pandas dataframe.
+    # TODO: Add error handling for file opening.
+    # TODO: Add error handling that check to make sure pluto or mars are the only "satellites" selected.  Make sure it is a valid config file.
+    simulation = BenrulesRealTimeSim(
+        time_step=time_step,
+        in_config_df = pd.read_csv("mars_sim_config.csv")
+    )
+    # Set offset so objects orbit the correct point.
     sunx = int(scr_width / 1.5)
     suny = int(scr_height / 2)
 
     sun_i_x = 185
     sun_i_y = 520
     while True:
-        # setting x and y radius of circle (will always be equal unless we want an isometric view
-        x_radius = 250
-        y_radius = 250
-
         # just runs through a large number of sequential frames with bogus data
+        # TODO: What does this mean?
         degree = 0
 
+        # Get next simulator state (positioning of all objects).
         current_positions, predicted_position = simulation.get_next_sim_state()
 
         while degree < 10000:
-
-            # print("speed:" , speed)
-
             # the functions below will be replaced by neural net output
             curr_time_step += 1
 
             if speed == 0.5 and pause == 0:
+                # Only advance simulation every other degree.
                 if degree % 2 == 0:
                     current_positions, predicted_position = simulation.get_next_sim_state()
 
             if speed == 1 and pause == 0:
+                # Advance simulation every degree
                 current_positions, predicted_position = simulation.get_next_sim_state()
 
             if speed == 2 and pause == 0:
+                # Advance simulation twice per degree
                 current_positions, predicted_position = simulation.get_next_sim_state()
                 current_positions, predicted_position = simulation.get_next_sim_state()
 
             if speed == 4 and pause == 0:
+                # Advance simulation 4 times every degree
                 current_positions, predicted_position = simulation.get_next_sim_state()
                 current_positions, predicted_position = simulation.get_next_sim_state()
                 current_positions, predicted_position = simulation.get_next_sim_state()
                 current_positions, predicted_position = simulation.get_next_sim_state()
 
-
-            # print("-----------------------------------------------------------------")
-            # print(curr_time_step , ":")
-            # print('(' + str(current_positions['earth'][0]) + " , " + str(current_positions['earth'][1]) + ")")
-
-            # print (sunmovex, sunmovey)
-
+            # Calculate the relative position of each body to the sun.
             x1 = int((current_positions['mercury'][0] - current_positions['sun'][0]) / zoom) + sunx
             y1 = int((current_positions['mercury'][1] - current_positions['sun'][1]) / zoom) + suny
-            xi1 = int((current_positions['mercury'][0] - current_positions['sun'][0])/ zoom*8) + sun_i_x
-            yi1 = int((current_positions['mercury'][1] - current_positions['sun'][1])/ zoom*8) + sun_i_y
+            xi1 = int((current_positions['mercury'][0] - current_positions['sun'][0])/ zoom_i*8) + sun_i_x
+            yi1 = int((current_positions['mercury'][1] - current_positions['sun'][1])/ zoom_i*8) + sun_i_y
             # print("x1:", x1, "...y1:", y1)
 
             x2 = int((current_positions['venus'][0] - current_positions['sun'][0]) / zoom) + sunx
             y2 = int((current_positions['venus'][1] - current_positions['sun'][1]) / zoom) + suny
-            xi2 = int((current_positions['venus'][0] - current_positions['sun'][0]) / zoom*8) + sun_i_x
-            yi2 = int((current_positions['venus'][1] - current_positions['sun'][1]) / zoom*8) + sun_i_y
+            xi2 = int((current_positions['venus'][0] - current_positions['sun'][0]) / zoom_i*8) + sun_i_x
+            yi2 = int((current_positions['venus'][1] - current_positions['sun'][1]) / zoom_i*8) + sun_i_y
             # print("x2:", x2, "...y2:", y2)
 
             x3 = int((current_positions['earth'][0] - current_positions['sun'][0]) / zoom) + sunx
             y3 = int((current_positions['earth'][1] - current_positions['sun'][1]) / zoom) + suny
-            xi3 = int((current_positions['earth'][0] - current_positions['sun'][0]) / zoom*8) + sun_i_x
-            yi3 = int((current_positions['earth'][1] - current_positions['sun'][1]) / zoom*8) + sun_i_y
+            xi3 = int((current_positions['earth'][0] - current_positions['sun'][0]) / zoom_i*8) + sun_i_x
+            yi3 = int((current_positions['earth'][1] - current_positions['sun'][1]) / zoom_i*8) + sun_i_y
             # print("x3:", x3, "...y3:", y3)
 
             x4 = int((current_positions['mars'][0] - current_positions['sun'][0]) / zoom) + sunx
             y4 = int((current_positions['mars'][1] - current_positions['sun'][1]) / zoom) + suny
-            xi4 = int((current_positions['mars'][0] - current_positions['sun'][0]) / zoom*8) + sun_i_x
-            yi4 = int((current_positions['mars'][1] - current_positions['sun'][1]) / zoom*8) + sun_i_y
+            xi4 = int((current_positions['mars'][0] - current_positions['sun'][0]) / zoom_i*8) + sun_i_x
+            yi4 = int((current_positions['mars'][1] - current_positions['sun'][1]) / zoom_i*8) + sun_i_y
 
             x5 = int((current_positions['jupiter'][0] - current_positions['sun'][0]) / zoom) + sunx
             y5 = int((current_positions['jupiter'][1] - current_positions['sun'][1]) / zoom) + suny
@@ -162,15 +187,18 @@ def orbits(screen, num_planets, tail_length, clock, scr_width, scr_height):
                 x9 = 0
                 y9 = 0
 
-            # setting the stage
+            # Setting the stage.
             screen.fill((0, 0, 0))
-            # sun(screen, int(scr_width / 1.5) - 35, int(scr_height / 2) - 35)
+            # Load and position the sun.
+            # TODO: Why are we loading the sun image every single frame?  Why not cache in memory outside the loop?
             sun_img = pygame.image.load('sun.png')
             sun_img = pygame.transform.scale(sun_img, (10, 10))
             screen.blit(sun_img, (int(scr_width / 1.5) - 5, int(scr_height / 2) - 5))
             screen.blit(sun_img, (sun_i_x-5, sun_i_y-5))
 
+            # If simulation not paused, then continue with moving the objects.
             if pause == 0:
+                # TODO: Could a queue just be used here?
                 # shifts all data points within the lists to the left to make room for the new trail data point
                 for j in range(0, num_planets):
                     for i in range(0, tail_length - 1):
@@ -188,6 +216,12 @@ def orbits(screen, num_planets, tail_length, clock, scr_width, scr_height):
                 x_track[6][tail_length - 1] = x7
                 x_track[7][tail_length - 1] = x8
                 x_track[8][tail_length - 1] = x9
+                x_track[9][tail_length - 1] = xi1
+                x_track[10][tail_length - 1] = xi2
+                x_track[11][tail_length - 1] = xi3
+                x_track[12][tail_length - 1] = xi4
+                # x_track[13][tail_length - 1] = sunmovex # place the sun position here
+
                 y_track[0][tail_length - 1] = y1
                 y_track[1][tail_length - 1] = y2
                 y_track[2][tail_length - 1] = y3
@@ -197,25 +231,26 @@ def orbits(screen, num_planets, tail_length, clock, scr_width, scr_height):
                 y_track[6][tail_length - 1] = y7
                 y_track[7][tail_length - 1] = y8
                 y_track[8][tail_length - 1] = y9
+                y_track[9][tail_length - 1] = yi1
+                y_track[10][tail_length - 1] = yi2
+                y_track[11][tail_length - 1] = yi3
+                y_track[12][tail_length - 1] = yi4
+                # y_track[13][tail_length - 1] = sunmovey # place sun position here
 
             if view == 0:
-                # iterates through the 2D list and draws the planet's trails
+                # Iterates through the 2D list and draws the planet's trails
                 for k in range(0, num_planets):
                     for j in range(1, tail_length - 1):
                         i = tail_length - j
                         if x_track[k][j - 1] != 0:
-                            pygame.draw.line(screen, (
-                                255 - 255 * (i / tail_length), 255 - 255 * (i / tail_length),
+                            pygame.draw.line(
+                                screen,
+                                (255 - 255 * (i / tail_length),
+                                255 - 255 * (i / tail_length),
                                 255 - 255 * (i / tail_length)),
-                                             [x_track[k][j], y_track[k][j]], [x_track[k][j - 1], y_track[k][j - 1]], 1)
-
-                            if k < 4 and j > tail_length - 10:
-                                pygame.draw.line(screen, (
-                                    255 - 255 * (i / 10), 255 - 255 * (i / 10),
-                                    255 - 255 * (i / 10)),
-                                                 [((x_track[k][j] - sunx)*8)+sun_i_x, ((y_track[k][j] - suny)*8)+sun_i_y], [((x_track[k][j - 1] - sunx)*8)+sun_i_x, ((y_track[k][j - 1] - suny)*8)+sun_i_y], 1)
-
-
+                                [x_track[k][j], y_track[k][j]],
+                                [x_track[k][j - 1], y_track[k][j - 1]],
+                                1)
 
                 pygame.draw.circle(screen, (255, 255, 0), [x2, y2], 5)
                 pygame.draw.circle(screen, (0, 255, 255), [x3, y3], 5)
@@ -233,25 +268,47 @@ def orbits(screen, num_planets, tail_length, clock, scr_width, scr_height):
                 pygame.draw.circle(screen, (0, 255, 0), [xi1, yi1], 5)
 
             else:
-                pygame.draw.circle(screen, (255, 255, 0), [x2, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (0, 255, 255), [x3, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (255, 255, 255), [x4, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (255, 0, 0), [x5, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (0, 255, 0), [x1, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (100, 50, 220), [x6, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (73, 155, 55), [x7, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (55, 75, 95), [x8, int(scr_height / 2)], 5)
-                pygame.draw.circle(screen, (255, 102, 255), [x9, int(scr_height / 2)], 5)
+                pygame.draw.circle(
+                    screen, (255, 255, 0), [x2, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (0, 255, 255), [x3, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (255, 255, 255), [x4, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (255, 0, 0), [x5, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (0, 255, 0), [x1, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (100, 50, 220), [x6, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (73, 155, 55), [x7, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (55, 75, 95), [x8, int(scr_height / 2)], 5
+                )
+                pygame.draw.circle(
+                    screen, (255, 102, 255), [x9, int(scr_height / 2)], 5
+                )
 
                 pygame.draw.circle(screen, (255, 255, 0), [xi2, sun_i_y], 5)
                 pygame.draw.circle(screen, (0, 255, 255), [xi3, sun_i_y], 5)
                 pygame.draw.circle(screen, (255, 255, 255), [xi4, sun_i_y], 5)
                 pygame.draw.circle(screen, (0, 255, 0), [xi1, sun_i_y], 5)
 
-
-
-            # updates the display with the new frame
-            states = menu(screen, [pause, view, speed,0,0, click_now, input_active, textbox_active, input_text], scr_width, scr_height)
+            # Updates the display with the new frame
+            states = menu(
+                screen,
+                [pause, view, speed,0,0, click_now, input_active,
+                 textbox_active, input_text],
+                scr_width,
+                scr_height
+            )
             pause = states[0]
             speed = states[2]
             input_active = states[6]
